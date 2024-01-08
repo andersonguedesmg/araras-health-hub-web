2f02b992e2ce3cf7f181c67dfc903ed617652edb
Anderson Guedes <anderson.guedes@outlook.com.br>
2024-01-08 19:33:06 -0300

    Revert "Instalação do PrimeNG"
    This reverts commit 913985f53cfbe3dea71815cf4aa7108e34d8e66c.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
