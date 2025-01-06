583b127b824121f1fae5f16f5e9f221cf2ba9463
Anderson Guedes <anderson.guedes@outlook.com.br>
2025-01-06 11:51:20 -0300

    feat(Destination): Add Componente e sua Rota

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,16 @@ import { Routes } from '@angular/router';
 import { NotFoundComponent } from './shared/pages/not-found/not-found.component';
 import { AboutComponent } from './shared/pages/about/about.component';
 import { HomeComponent } from './shared/pages/home/home.component';
+import { DestinationListComponent } from './features/destination/components/destination-list/destination-list.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+  },
+  {
+    path: 'destinos',
+    component: DestinationListComponent,
   },
   {
     path: 'sobre',
