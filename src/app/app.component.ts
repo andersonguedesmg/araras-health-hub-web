32ec770be4045be50b5577ecb6dde191333131c2
Anderson Guedes <anderson.guedes@outlook.com.br>
2025-01-06 17:28:22 -0300

    build(common): Troca do Bootstrap pelo PrimeNG

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PrimeNG } from 'primeng/config';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/components/header/header.component';
 
@@ -8,6 +9,11 @@ import { HeaderComponent } from './shared/components/header/header.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'araras-health-hub-web';
+export class AppComponent implements OnInit {
+
+  constructor(private primeng: PrimeNG) { }
+
+  ngOnInit() {
+    this.primeng.ripple.set(true);
+  }
 }
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,12 +1,110 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem, PrimeIcons } from 'primeng/api';
+import { Menubar } from 'primeng/menubar';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  imports: [Menubar],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  items: MenuItem[] | undefined;
 
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Home',
+        icon: PrimeIcons.HOME,
+        routerLink: '/',
+      },
+      {
+        label: 'Usuários',
+        icon: PrimeIcons.USER,
+        routerLink: '/usuarios',
+      },
+      {
+        label: 'Destinos',
+        icon: PrimeIcons.ADDRESS_BOOK,
+        routerLink: '/destinos',
+      },
+      {
+        label: 'Fornecedores',
+        icon: PrimeIcons.SHOP,
+        routerLink: '/fornecedores',
+      },
+      {
+        label: 'Produtos',
+        icon: PrimeIcons.SHOPPING_BAG,
+        routerLink: '/produtos',
+      },
+      {
+        label: 'Estoque',
+        icon: PrimeIcons.WAREHOUSE,
+        routerLink: '/estoque',
+      },
+      {
+        label: 'Pedidos',
+        icon: PrimeIcons.SHOPPING_CART,
+        items: [
+          {
+            label: 'Criado',
+            icon: PrimeIcons.PAPERCLIP,
+            routerLink: '/pedidos',
+          },
+          {
+            label: 'Aguardando Aprovação',
+            icon: PrimeIcons.CLOCK,
+            routerLink: '/pedidos',
+          },
+          {
+            label: 'Aprovado',
+            icon: PrimeIcons.LIST_CHECK,
+            routerLink: '/pedidos',
+          },
+          {
+            label: 'Em Processamento',
+            icon: PrimeIcons.CART_ARROW_DOWN,
+            routerLink: '/pedidos',
+          },
+          {
+            label: 'Separado',
+            icon: PrimeIcons.HOURGLASS,
+            routerLink: '/pedidos',
+          },
+          {
+            label: 'Em Transito',
+            icon: PrimeIcons.TRUCK,
+            routerLink: '/pedidos',
+          },
+          {
+            label: 'Finalizados',
+            icon: PrimeIcons.TROPHY,
+            items: [
+              {
+                label: 'Entregue',
+                icon: PrimeIcons.CHECK_CIRCLE,
+                routerLink: '/pedidos',
+              },
+              {
+                label: 'Cancelado',
+                icon: PrimeIcons.TIMES_CIRCLE,
+                routerLink: '/pedidos',
+              },
+              {
+                label: 'Devolvido',
+                icon: PrimeIcons.UNDO,
+                routerLink: '/pedidos',
+              }
+            ]
+          }
+        ]
+      },
+      {
+        label: 'Sobre',
+        icon: PrimeIcons.COMPASS,
+        routerLink: '/sobre',
+      }
+    ];
+  }
 }
