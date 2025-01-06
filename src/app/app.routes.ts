35cd54c31498a78ba062dadbfe22132ece70b3c8
Anderson Guedes <anderson.guedes@outlook.com.br>
2025-01-06 11:13:27 -0300

    build(common): Add páginas Home, About e NotFound

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { NotFoundComponent } from './shared/pages/not-found/not-found.component';
+import { AboutComponent } from './shared/pages/about/about.component';
+import { HomeComponent } from './shared/pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'sobre',
+    component: AboutComponent,
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+  },
+];
