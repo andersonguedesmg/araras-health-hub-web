4b2bc44e2ce284ebe4b25995c8b69a615ecbed62
Anderson Guedes <anderson.guedes@outlook.com.br>
2025-01-15 08:56:51 -0300

    feat(supplier): Add Breadcrumb

--- src/app/features/supplier/components/supplier-list/supplier-list.component.ts
+++ src/app/features/supplier/components/supplier-list/supplier-list.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BreadcrumbComponent } from '../../../../shared/components/breadcrumb/breadcrumb.component';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-supplier-list',
-  imports: [],
+  imports: [BreadcrumbComponent],
   templateUrl: './supplier-list.component.html',
   styleUrl: './supplier-list.component.scss'
 })
-export class SupplierListComponent {
+export class SupplierListComponent implements OnInit {
+  itemsBreadcrumb: MenuItem[] = [{ label: 'Fornecedores' },];
 
+  constructor() { }
+
+  ngOnInit() { }
 }
