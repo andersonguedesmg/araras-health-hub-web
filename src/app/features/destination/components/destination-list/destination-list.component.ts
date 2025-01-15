101075cbe3aa1dbfbec4a168d257b9c261f868e9
Anderson Guedes <anderson.guedes@outlook.com.br>
2025-01-15 08:49:32 -0300

    feat(destination): Add Breadcrumb

--- src/app/features/destination/components/destination-list/destination-list.component.ts
+++ src/app/features/destination/components/destination-list/destination-list.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BreadcrumbComponent } from '../../../../shared/components/breadcrumb/breadcrumb.component';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-destination-list',
-  imports: [],
+  imports: [BreadcrumbComponent],
+  providers: [],
   templateUrl: './destination-list.component.html',
   styleUrl: './destination-list.component.scss'
 })
-export class DestinationListComponent {
+export class DestinationListComponent implements OnInit {
+  itemsBreadcrumb: MenuItem[] = [{ label: 'Destinos' },];
 
+  constructor() { }
+
+  ngOnInit() { }
 }
