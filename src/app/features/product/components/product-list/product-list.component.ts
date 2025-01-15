c5e4a0ab7fc01629b626ad99ce7020063552ddeb
Anderson Guedes <anderson.guedes@outlook.com.br>
2025-01-15 09:01:24 -0300

    feat(product): Add Breadcrumb

--- src/app/features/product/components/product-list/product-list.component.ts
+++ src/app/features/product/components/product-list/product-list.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BreadcrumbComponent } from '../../../../shared/components/breadcrumb/breadcrumb.component';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-product-list',
-  imports: [],
+  imports: [BreadcrumbComponent],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.scss'
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
+  itemsBreadcrumb: MenuItem[] = [{ label: 'Produtos' },];
 
+  constructor() { }
+
+  ngOnInit() { }
 }
