1c9887e533b4d415ed5a3dd97e99c8c011fb84b4
Anderson Guedes <anderson.guedes@outlook.com.br>
2025-01-15 08:53:41 -0300

    feat(user): Add Breadcrumb

--- src/app/features/user/components/user-list/user-list.component.ts
+++ src/app/features/user/components/user-list/user-list.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BreadcrumbComponent } from '../../../../shared/components/breadcrumb/breadcrumb.component';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-user-list',
-  imports: [],
+  imports: [BreadcrumbComponent],
   templateUrl: './user-list.component.html',
   styleUrl: './user-list.component.scss'
 })
-export class UserListComponent {
+export class UserListComponent implements OnInit {
+  itemsBreadcrumb: MenuItem[] = [{ label: 'Usuários' },];
 
+  constructor() { }
+
+  ngOnInit() { }
 }
