8d8d19e412497251261d42e6f452776b8a165e53
Anderson Guedes <anderson.guedes@outlook.com.br>
2025-04-12 09:08:01 -0300

    feat(supplier): Add getNames

--- src/app/features/supplier/services/supplier.service.ts
+++ src/app/features/supplier/services/supplier.service.ts
@@ -4,6 +4,7 @@ import { firstValueFrom } from 'rxjs';
 import { ApiResponse } from '../../../shared/interfaces/apiResponse';
 import { Supplier } from '../interfaces/supplier';
 import { ApiConfigService } from '../../../shared/services/api-config.service';
+import { SelectOptions } from '../../../shared/interfaces/select-options';
 
 @Injectable({
   providedIn: 'root'
@@ -34,5 +35,10 @@ export class SupplierService {
   async changeStatusSupplier(supplierId: number, supplier: Supplier): Promise<ApiResponse<Supplier>> {
     const url = this.apiConfig.getSupplierUrl(`changeStatus/${supplierId}`);
     return firstValueFrom(this.http.patch<ApiResponse<Supplier>>(url, supplier));
+  }
+
+  async getAllSupplierNames(): Promise<ApiResponse<SelectOptions<number>[]>> {
+    const url = this.apiConfig.getSupplierUrl('getNames');
+    return firstValueFrom(this.http.get<ApiResponse<SelectOptions<number>[]>>(url));
   }
 }
