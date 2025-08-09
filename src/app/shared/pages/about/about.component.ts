8d26ab7d8fb81de86ced190a77f65b97feec6ca5
Anderson Guedes <anderson.guedes@outlook.com.br>
2025-08-09 19:23:20 -0300

    refactor(common): sobre

--- src/app/shared/pages/about/about.component.ts
+++ src/app/shared/pages/about/about.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { ToolbarModule } from 'primeng/toolbar';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [
+    CommonModule,
+    RouterModule,
+    ToolbarModule,
+    ButtonModule
+  ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
