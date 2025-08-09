73b1ef7eeda4abff0e415598d5c5f11d88b7ef6d
Anderson Guedes <anderson.guedes@outlook.com.br>
2025-08-09 20:18:16 -0300

    feat(common): implemente tela 403

--- src/app/shared/pages/unauthorized/unauthorized.component.ts
+++ src/app/shared/pages/unauthorized/unauthorized.component.ts
@@ -1,8 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-unauthorized',
-  imports: [],
+  imports: [
+    CommonModule,
+    RouterModule,
+    ButtonModule
+  ],
   templateUrl: './unauthorized.component.html',
   styleUrl: './unauthorized.component.scss'
 })
