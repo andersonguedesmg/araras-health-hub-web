90194e5a82f5a11f9f82ad013989614e6e0601f9
Anderson Guedes <anderson.guedes@outlook.com.br>
2025-10-03 10:06:07 -0300

    refactor(facility): add BaseComponent

--- src/app/features/facility/components/facility-list/facility-list.component.ts
+++ src/app/features/facility/components/facility-list/facility-list.component.ts
@@ -1,8 +1,8 @@
-import { ChangeDetectorRef, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
 import { BreadcrumbComponent } from '../../../../shared/components/breadcrumb/breadcrumb.component';
 import { MenuItem, MessageService } from 'primeng/api';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule, FormsModule, FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule, FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { DialogModule } from 'primeng/dialog';
@@ -10,7 +10,6 @@ import { IconFieldModule } from 'primeng/iconfield';
 import { InputIconModule } from 'primeng/inputicon';
 import { InputTextModule } from 'primeng/inputtext';
 import { SelectModule } from 'primeng/select';
-import { Table } from 'primeng/table';
 import { TagModule } from 'primeng/tag';
 import { ToastModule } from 'primeng/toast';
 import { ToolbarModule } from 'primeng/toolbar';
@@ -22,19 +21,19 @@ import { SpinnerComponent } from '../../../../shared/components/spinner/spinner.
 import { ToastComponent } from '../../../../shared/components/toast/toast.component';
 import { FormMode } from '../../../../shared/enums/form-mode.enum';
 import { ConfirmMode } from '../../../../shared/enums/confirm-mode.enum';
-import { Column, ExportColumn } from '../../../../shared/utils/p-table.utils';
+import { Column } from '../../../../shared/utils/p-table.utils';
 import { getSeverity, getStatus } from '../../../../shared/utils/status.utils';
 import { StatusOptions } from '../../../../shared/constants/status-options.constants';
-import { ApiResponse } from '../../../../shared/interfaces/api-response';
 import { ConfirmMessages, ToastMessages } from '../../../../shared/constants/messages.constants';
 import { ToastSeverities, ToastSummaries } from '../../../../shared/constants/toast.constants';
-import { HttpStatus } from '../../../../shared/enums/http-status.enum';
 import { debounceTime, firstValueFrom, Observable, Subject, Subscription, switchMap } from 'rxjs';
 import { HasRoleDirective } from '../../../../core/directives/has-role.directive';
 import { DialogComponent } from '../../../../shared/components/dialog/dialog.component';
 import { TableHeaderComponent } from '../../../../shared/components/table-header/table-header.component';
 import { TableComponent } from '../../../../shared/components/table/table.component';
 import { InputMask } from 'primeng/inputmask';
+import { FormHelperService } from '../../../../core/services/form-helper.service';
+import { BaseComponent } from '../../../../core/components/base/base.component';
 
 @Component({
   selector: 'app-facility-list',
@@ -67,14 +66,9 @@ import { InputMask } from 'primeng/inputmask';
   templateUrl: './facility-list.component.html',
   styleUrl: './facility-list.component.scss'
 })
-export class FacilityListComponent implements OnInit, OnDestroy {
-  @ViewChild(ToastComponent) toastComponent!: ToastComponent;
-  @ViewChild(ConfirmDialogComponent) confirmDialog!: ConfirmDialogComponent;
-
+export class FacilityListComponent extends BaseComponent implements OnInit, OnDestroy {
   itemsBreadcrumb: MenuItem[] = [{ label: 'Administração' }, { label: 'Unidades' }];
   title: string = 'Unidades';
-
-  isLoading = false;
 
   FormMode = FormMode;
   ConfirmMode = ConfirmMode;
@@ -93,8 +87,6 @@ export class FacilityListComponent implements OnInit, OnDestroy {
   headerText = '';
 
   cols!: Column[];
-  selectedColumns!: Column[];
-  exportColumns!: ExportColumn[];
 
   private formLabels: { [key: string]: string; } = {
     name: 'Nome da Unidade',
@@ -118,7 +110,13 @@ export class FacilityListComponent implements OnInit, OnDestroy {
   private subscriptions: Subscription = new Subscription();
   totalRecords = 0;
 
-  constructor(private cd: ChangeDetectorRef, private facilityService: FacilityService, private fb: FormBuilder) {
+  constructor(
+    private cd: ChangeDetectorRef,
+    private facilityService: FacilityService,
+    private fb: FormBuilder,
+    private formHelperService: FormHelperService,
+  ) {
+    super();
     this.facilityForm = this.fb.group({
       id: [{ value: null, disabled: true }],
       name: ['', Validators.required],
@@ -192,8 +190,6 @@ export class FacilityListComponent implements OnInit, OnDestroy {
       { field: 'phone', header: 'TELEFONE' },
       { field: 'isActive', header: 'STATUS' },
     ];
-    this.exportColumns = this.cols.map((col) => ({ title: col.header, dataKey: col.field }));
-    this.selectedColumns = this.cols;
   }
 
   loadFacilities(event: any) {
@@ -293,147 +289,29 @@ export class FacilityListComponent implements OnInit, OnDestroy {
   async saveFacility(): Promise<void> {
     this.formSubmitted = true;
     if (this.validateForm()) {
-      this.confirmMessage = this.formMode === FormMode.Create ? ConfirmMessages.CREATE_FACILITY : ConfirmMessages.UPDATE_FACILITY;
-      this.confirmDialog.message = this.confirmMessage;
-      this.confirmDialog.show();
-
-      try {
-        await firstValueFrom(this.confirmDialog.confirmed);
-        this.isLoading = true;
-        const facility: Facility = this.facilityForm.getRawValue();
-
-        const apiCall$ = this.formMode === FormMode.Create
-          ? this.facilityService.createFacility(facility)
-          : this.facilityService.updateFacility(facility, facility.id);
-
-        const response = await firstValueFrom(apiCall$);
-
-        this.isLoading = false;
-        this.handleApiResponse(response, ToastMessages.SUCCESS_OPERATION);
-        this.hideDialog();
-
-      } catch (error: any) {
-        this.isLoading = false;
-        if (error.message !== 'cancel') {
-          this.handleApiError(error);
-        }
-      }
+      const confirmMsg = this.formMode === FormMode.Create ? ConfirmMessages.CREATE_FACILITY : ConfirmMessages.UPDATE_FACILITY;
+      const facility = this.facilityForm.getRawValue();
+      const apiCall = this.formMode === FormMode.Create
+        ? firstValueFrom(this.facilityService.createFacility(facility))
+        : firstValueFrom(this.facilityService.updateFacility(facility, facility.id));
+      await this.handleApiCall(apiCall, confirmMsg, ToastMessages.SUCCESS_OPERATION);
+      this.hideDialog();
     }
   }
 
   async changeStatusFacility(facilityId: number, facility: Facility): Promise<void> {
-    this.confirmMessage = facility.isActive ? ConfirmMessages.DISABLE_FACILITY : ConfirmMessages.ACTIVATE_FACILITY;
-    this.confirmDialog.message = this.confirmMessage;
-    this.confirmDialog.show();
-
-    try {
-      await firstValueFrom(this.confirmDialog.confirmed);
-      this.isLoading = true;
-
-      const changeFacilityIsActive = this.changeIsActive(facility);
-      const response = await firstValueFrom(this.facilityService.changeStatusFacility(facilityId, changeFacilityIsActive));
-
-      this.isLoading = false;
-      this.handleApiResponse(response, ToastMessages.SUCCESS_OPERATION);
-
-    } catch (error: any) {
-      this.isLoading = false;
-      if (error.message !== 'cancel') {
-        this.handleApiError(error);
-      }
-    }
+    const confirmMsg = facility.isActive ? ConfirmMessages.DISABLE_FACILITY : ConfirmMessages.ACTIVATE_FACILITY;
+    const changeFacilityIsActive = this.changeIsActive(facility);
+    const apiCall = firstValueFrom(this.facilityService.changeStatusFacility(facilityId, changeFacilityIsActive));
+    await this.handleApiCall(apiCall, confirmMsg, ToastMessages.SUCCESS_OPERATION);
   }
 
   async deleteFacility(facilityId: number): Promise<void> {
-    this.confirmDialog.message = ConfirmMessages.DELETE_FACILITY;
-    this.confirmDialog.show();
-
-    try {
-      await firstValueFrom(this.confirmDialog.confirmed);
-      this.isLoading = true;
-
-      const response = await firstValueFrom(this.facilityService.deleteFacility(facilityId));
-
-      this.isLoading = false;
-      this.handleApiResponse(response, ToastMessages.SUCCESS_OPERATION);
-
-    } catch (error: any) {
-      this.isLoading = false;
-      if (error.message !== 'cancel') {
-        this.handleApiError(error);
-      }
-    }
+    const apiCall = firstValueFrom(this.facilityService.deleteFacility(facilityId));
+    await this.handleApiCall(apiCall, ConfirmMessages.DELETE_FACILITY, ToastMessages.SUCCESS_OPERATION);
   }
 
   private validateForm(): boolean {
-    if (this.facilityForm.valid) {
-      return true;
-    }
-
-    const invalidControls = this.findInvalidControlsRecursive(this.facilityForm);
-    const invalidFields = invalidControls.map(control => {
-      const controlName = this.getFormControlName(control);
-      return controlName;
-    });
-
-    const invalidFieldsMessage = invalidFields.length > 0
-      ? `Por favor, preencha os seguintes campos: ${invalidFields.join(', ')}.`
-      : ToastMessages.REQUIRED_FIELDS;
-
-    this.toastComponent.showMessage(ToastSeverities.ERROR, ToastSummaries.ERROR, invalidFieldsMessage);
-    return false;
-  }
-
-  private findInvalidControlsRecursive(form: FormGroup | AbstractControl): AbstractControl[] {
-    const invalidControls: AbstractControl[] = [];
-    if (form instanceof FormGroup) {
-      for (const name in form.controls) {
-        const control = form.controls[name];
-        if (control.invalid) {
-          invalidControls.push(control);
-        } else if (control instanceof FormGroup) {
-          invalidControls.push(...this.findInvalidControlsRecursive(control));
-        }
-      }
-    }
-    return invalidControls;
-  }
-
-  private getFormControlName(control: AbstractControl): string {
-    const parent = control.parent;
-    if (parent instanceof FormGroup) {
-      const formGroup = parent as FormGroup;
-      for (const name in formGroup.controls) {
-        if (control === formGroup.controls[name]) {
-          return this.formLabels[name] || name.replace(/([A-Z])/g, ' $1').replace(/^./, (str) => str.toUpperCase());
-        }
-      }
-    }
-    return '';
-  }
-
-  private handleApiResponse(response: ApiResponse<any>, successMessage: string) {
-    if (response.success) {
-      this.toastComponent.showMessage(ToastSeverities.SUCCESS, ToastSummaries.SUCCESS, response.message || successMessage);
-    } else {
-      this.toastComponent.showMessage(ToastSeverities.ERROR, ToastSummaries.ERROR, response.message || ToastMessages.UNEXPECTED_ERROR);
-    }
-  }
-
-  private handleApiError(error: any) {
-    if (error.error && error.error.statusCode === HttpStatus.NotFound) {
-      this.toastComponent.showMessage(ToastSeverities.INFO, ToastSummaries.INFO, error.error.message);
-    } else if (error.error && error.error.message) {
-      this.toastComponent.showMessage(ToastSeverities.ERROR, ToastSummaries.ERROR, error.error.message);
-    } else {
-      this.toastComponent.showMessage(ToastSeverities.ERROR, ToastSummaries.ERROR, ToastMessages.UNEXPECTED_ERROR);
-    }
-  }
-
-  changeIsActive(objeto: any) {
-    if (objeto && typeof objeto === 'object' && 'isActive' in objeto) {
-      objeto.isActive = !objeto.isActive;
-    }
-    return objeto;
+    return this.validateFormAndShowErrors(this.facilityForm, this.formHelperService, this.formLabels);
   }
 }
