6c4689501ba82fc557baae4d54e876e3961c99b1
Anderson Guedes <anderson.guedes@outlook.com.br>
2026-06-11 08:34:10 -0300

    style(shared): reestilizar componente de toast para padrão semântico sólido - Mudar fundo do toast para cores sólidas - Remover classes de cores específicas dos ícones - Incrementar tempo padrão de exibição do toast na tela para 5 segundos

--- src/app/shared/components/toast/toast.component.ts
+++ src/app/shared/components/toast/toast.component.ts
@@ -1,23 +1,14 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MessageService } from 'primeng/api';
 import { ToastModule } from 'primeng/toast';
 
 @Component({
   selector: 'app-toast',
-  imports: [ToastModule, CommonModule],
+  standalone: true,
+  imports: [ToastModule],
   templateUrl: './toast.component.html',
-  styleUrl: './toast.component.scss'
+  styleUrl: './toast.component.scss',
 })
 export class ToastComponent {
-  constructor(public messageService: MessageService) { }
-
-  showMessage(severity: 'success' | 'info' | 'warn' | 'error' | 'secondary' | 'contrast', summary: string, detail: string): void {
-    this.messageService.clear();
-    this.messageService.add({
-      severity: severity,
-      summary: summary,
-      detail: detail,
-    });
-  }
+  readonly messageService = inject(MessageService);
 }
