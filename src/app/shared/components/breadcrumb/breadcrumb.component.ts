9e5f191aafa3cb9b40976f5d2d041cf2605b40b8
Anderson Guedes <anderson.guedes@outlook.com.br>
2026-06-12 15:49:12 -0300

    style(shared): reestilizar componente de breadcrumb

--- src/app/shared/components/breadcrumb/breadcrumb.component.ts
+++ src/app/shared/components/breadcrumb/breadcrumb.component.ts
@@ -1,23 +1,24 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, computed, input, ViewEncapsulation } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { MenuItem, PrimeIcons } from 'primeng/api';
-import { Breadcrumb } from 'primeng/breadcrumb';
+import { BreadcrumbModule } from 'primeng/breadcrumb';
 
 @Component({
   selector: 'app-breadcrumb',
-  imports: [Breadcrumb],
+  standalone: true,
+  imports: [CommonModule, BreadcrumbModule, RouterModule],
   templateUrl: './breadcrumb.component.html',
-  styleUrl: './breadcrumb.component.scss'
+  styleUrl: './breadcrumb.component.scss',
+  encapsulation: ViewEncapsulation.None,
 })
-export class BreadcrumbComponent implements OnInit {
-  @Input({ required: true }) itemsBreadcrumb!: MenuItem[];
+export class BreadcrumbComponent {
+  itemsBreadcrumb = input.required<MenuItem[]>();
 
-  items: MenuItem[] | undefined;
-  home: MenuItem | undefined;
+  protected readonly home: MenuItem = {
+    icon: PrimeIcons.HOME,
+    routerLink: '/',
+  };
 
-  constructor() { }
-
-  ngOnInit() {
-    this.home = { icon: PrimeIcons.HOME, routerLink: '/' };
-    this.items = this.itemsBreadcrumb;
-  }
+  protected items = computed(() => this.itemsBreadcrumb());
 }
