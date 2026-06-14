5c050ecd26b8bc944dddaac41aaa605e2f994289
Anderson Guedes <anderson.guedes@outlook.com.br>
2026-06-14 10:07:14 -0300

    style(shared): reestilizar componente de dialog

--- src/app/shared/components/dialog/dialog.component.ts
+++ src/app/shared/components/dialog/dialog.component.ts
@@ -1,39 +1,33 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { DialogModule } from 'primeng/dialog';
+import { Component, input, model, output, ViewEncapsulation } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
+import { DialogModule } from 'primeng/dialog';
 import { FormMode } from '../../enums/form-mode.enum';
 
 @Component({
   selector: 'app-dialog',
+  standalone: true,
   imports: [CommonModule, DialogModule, ButtonModule],
   templateUrl: './dialog.component.html',
-  styleUrl: './dialog.component.scss'
+  styleUrl: './dialog.component.scss',
+  encapsulation: ViewEncapsulation.None,
 })
 export class DialogComponent {
-  @Input() visible: boolean = false;
-  @Input() formMode: FormMode.Create | FormMode.Update | FormMode.Detail = FormMode.Create;
-  @Input() headerText: string = '';
-  @Input() showSaveButton: boolean = true;
-  @Input() saveButtonLabel: string = 'Salvar';
-  @Input() cancelButtonLabel: string = 'Cancelar';
+  visible = model<boolean>(false);
 
-  @Output() visibleChange = new EventEmitter<boolean>();
-  @Output() onSave = new EventEmitter<void>();
-  @Output() onCancel = new EventEmitter<void>();
+  formMode = input<FormMode>(FormMode.Create);
+  headerText = input<string>('');
+  showSaveButton = input<boolean>(true);
+  saveButtonLabel = input<string>('Salvar');
+  cancelButtonLabel = input<string>('Cancelar');
 
-  FormMode = FormMode;
+  onSave = output<void>();
+  onCancel = output<void>();
 
-  hideDialog(): void {
-    this.visibleChange.emit(false);
-  }
+  protected FormMode = FormMode;
 
-  save(): void {
-    this.onSave.emit();
-  }
-
-  cancel(): void {
-    this.visibleChange.emit(false);
+  protected handleCancel(): void {
+    this.visible.set(false);
     this.onCancel.emit();
   }
 }
