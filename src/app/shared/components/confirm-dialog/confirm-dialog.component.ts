f2f558bd416441d86c8e1bd0951ae05d5e775f08
Anderson Guedes <anderson.guedes@outlook.com.br>
2026-06-14 09:58:44 -0300

    style(confirm-dialog): reestilizar componente de toast

--- src/app/shared/components/confirm-dialog/confirm-dialog.component.ts
+++ src/app/shared/components/confirm-dialog/confirm-dialog.component.ts
@@ -1,41 +1,44 @@
-import { Component, Input } from '@angular/core';
+import { Component, input, ViewEncapsulation } from '@angular/core';
 import { ConfirmationService } from 'primeng/api';
 import { ConfirmDialogModule } from 'primeng/confirmdialog';
 import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-confirm-dialog',
+  standalone: true,
   imports: [ConfirmDialogModule],
   providers: [ConfirmationService],
+  encapsulation: ViewEncapsulation.None,
   templateUrl: './confirm-dialog.component.html',
-  styleUrl: './confirm-dialog.component.scss'
+  styleUrl: './confirm-dialog.component.scss',
 })
 export class ConfirmDialogComponent {
-  @Input() header = 'Confirmação';
-  @Input() message = 'Tem certeza que deseja continuar?';
-  @Input() icon = 'pi pi-exclamation-triangle';
-  @Input() acceptLabel = 'Sim';
-  @Input() rejectLabel = 'Não';
-  @Input() acceptIcon = 'pi pi-check';
-  @Input() rejectIcon = 'pi pi-times';
+  header = input<string>('Confirmação');
+  message = input<string>('Tem certeza que deseja continuar?');
+  icon = input<string>('pi pi-exclamation-triangle');
+  acceptLabel = input<string>('Sim');
+  rejectLabel = input<string>('Não');
+  acceptIcon = input<string>('pi pi-check');
+  rejectIcon = input<string>('pi pi-times');
 
   private confirmationSubject = new Subject<boolean>();
 
-  constructor(private confirmationService: ConfirmationService) { }
+  constructor(private confirmationService: ConfirmationService) {}
 
-  show(): Subject<boolean> {
+  public show(customMessage?: string, customHeader?: string): Subject<boolean> {
     this.confirmationSubject = new Subject<boolean>();
 
     this.confirmationService.confirm({
-      message: this.message,
-      header: this.header,
-      icon: this.icon,
+      message: customMessage || this.message(),
+      header: customHeader || this.header(),
+      icon: this.icon(),
       accept: () => {
         this.confirmationSubject.next(true);
         this.confirmationSubject.complete();
       },
       reject: () => {
-        this.confirmationSubject.error('cancel');
+        this.confirmationSubject.next(false);
+        this.confirmationSubject.complete();
       },
     });
 
