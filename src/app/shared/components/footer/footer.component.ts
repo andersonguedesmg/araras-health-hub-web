0169b0bb757d2df6c4390ff333d6e1b489f59851
Anderson Guedes <anderson.guedes@outlook.com.br>
2026-07-11 12:15:36 -0300

    refactor(shared): modernizar controle do footer e aplicar ano dinâmico

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,14 +1,24 @@
-import { CommonModule } from '@angular/common';
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule],
-  encapsulation: ViewEncapsulation.None,
+  imports: [],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
+  host: {
+    class: 'block w-full',
+  },
 })
 export class FooterComponent {
-  readonly currentYear: number = new Date().getFullYear();
+  private readonly START_YEAR = 2025;
+  private readonly currentYear = signal<number>(new Date().getFullYear());
+
+  readonly copyrightText = computed<string>(() => {
+    const year = this.currentYear();
+    if (year > this.START_YEAR) {
+      return `${this.START_YEAR} - ${year}`;
+    }
+    return `${this.START_YEAR}`;
+  });
 }
