36a5a247e159f8e8e88a9569fd9727e846e688a1
Anderson Guedes <anderson.guedes@outlook.com.br>
2026-07-13 20:40:10 -0300

    refactor(employees): simplificar tabela de funcionários com injeção moderna e signals

--- src/app/features/employee/components/employee-table/employee-table.component.ts
+++ src/app/features/employee/components/employee-table/employee-table.component.ts
@@ -1,9 +1,9 @@
-import { CommonModule } from '@angular/common';
-import { Component, input, output, ViewEncapsulation } from '@angular/core';
+import { Component, inject, input, output } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { TableLazyLoadEvent } from 'primeng/table';
 import { TagModule } from 'primeng/tag';
 import { TooltipModule } from 'primeng/tooltip';
+import { AuthService } from '../../../../core/services/auth.service';
 import { TableToolbarComponent } from '../../../../shared/components/table-toolbar/table-toolbar.component';
 import { TableComponent } from '../../../../shared/components/table/table.component';
 import { getSeverity, getStatus } from '../../../../shared/utils/status.utils';
@@ -13,36 +13,35 @@ import { Employee } from '../../interfaces/employee';
   selector: 'app-employee-table',
   standalone: true,
   imports: [
-    CommonModule,
     ButtonModule,
     TagModule,
     TooltipModule,
     TableComponent,
     TableToolbarComponent,
   ],
-  encapsulation: ViewEncapsulation.None,
   templateUrl: './employee-table.component.html',
   styleUrl: './employee-table.component.scss',
 })
 export class EmployeeTableComponent {
-  value = input<Employee[]>([]);
-  totalRecords = input<number>(0);
-  loading = input<boolean>(false);
-  rows = input<number>(5);
-  first = input<number>(0);
+  private readonly authService = inject(AuthService);
 
-  onLazyLoad = output<TableLazyLoadEvent>();
-  onSearch = output<string>();
-  onAdd = output<void>();
-  onPdfClick = output<void>();
-  onEdit = output<Employee>();
-  onDetail = output<Employee>();
-  onChangeStatus = output<Employee>();
+  readonly value = input<Employee[]>([]);
+  readonly totalRecords = input<number>(0);
+  readonly loading = input<boolean>(false);
+  readonly rows = input<number>(5);
+  readonly first = input<number>(0);
+
+  readonly onLazyLoad = output<TableLazyLoadEvent>();
+  readonly onSearch = output<string>();
+  readonly onAdd = output<void>();
+  readonly onPdfClick = output<void>();
+  readonly onEdit = output<Employee>();
+  readonly onDetail = output<Employee>();
+  readonly onChangeStatus = output<Employee>();
 
   protected readonly getSeverity = getSeverity;
   protected readonly getStatus = getStatus;
 
-  protected hasManagementPermission(): boolean {
-    return true;
-  }
+  protected readonly hasManagementPermission =
+    this.authService.hasManagementPermission;
 }
