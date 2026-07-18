2cee95fdbb14e7cbd39e5a7ff4224212c8818007
Anderson Guedes <anderson.guedes@outlook.com.br>
2026-07-18 19:38:38 -0300

    feat(facilities): implementar getFacilityPagedOptions

--- src/app/features/facility/services/facility.service.ts
+++ src/app/features/facility/services/facility.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
-import { Observable, tap } from 'rxjs';
+import { map, Observable, tap } from 'rxjs';
 import { ApiDropdownItem } from '../../../shared/interfaces/api-dropdown-item';
 import { ApiResponse } from '../../../shared/interfaces/api-response';
 import { SelectOptions } from '../../../shared/interfaces/select-options';
@@ -102,19 +102,36 @@ export class FacilityService {
     return this.http.get<ApiResponse<FacilityProfile>>(url);
   }
 
-  public getFacilityOptions(): Observable<SelectOptions<number>[]> {
+  public getFacilityPagedOptions(
+    pageNumber: number,
+    pageSize: number,
+    searchTerm: string = '',
+    isActive?: boolean,
+  ): Observable<ApiResponse<SelectOptions<number>[]>> {
     const url = this.apiConfig.getUrl('facilities/dropdown');
-    return this.http.get<ApiResponse<ApiDropdownItem[]>>(url).pipe(
-      tap({
-        next: (response) => {
-          return (
-            response?.data?.map((item) => ({
-              label: item.name,
-              value: item.id,
-            })) || []
-          );
-        },
+
+    let params = new HttpParams()
+      .set('pageNumber', pageNumber.toString())
+      .set('pageSize', pageSize.toString())
+      .set('searchTerm', searchTerm);
+
+    if (isActive !== undefined) {
+      params = params.set('isActive', isActive.toString());
+    }
+
+    return this.http.get<ApiResponse<ApiDropdownItem[]>>(url, { params }).pipe(
+      map((response) => {
+        const mappedData: SelectOptions<number>[] =
+          response?.data?.map((item) => ({
+            label: item.label,
+            value: item.id,
+          })) || [];
+
+        return {
+          ...response,
+          data: mappedData,
+        };
       }),
-    ) as unknown as Observable<SelectOptions<number>[]>;
+    );
   }
 }
