287c8ca060f48ce6311928e23475fbccf997d58e
Anderson Guedes <anderson.guedes@outlook.com.br>
2026-09-12 10:09:52 -0300

    chore(config): configurar linting e formatação e otimizar builds do projeto - Adicionados arquivos de configuração do Prettier (.prettierrc e .prettierignore) - Adicionada configuração plana do ESLint para Angular (eslint.config.js) - Atualizado angular.json para forcar componentes standalone por padrão nos schematics

---
+++ eslint.config.js
@@ -0,0 +1,39 @@
+const eslint = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+const angular = require('angular-eslint');
+const eslintConfigPrettier = require('eslint-config-prettier');
+
+module.exports = tseslint.config(
+  {
+    ignores: ['dist/**', '.angular/**', 'node_modules/**'],
+  },
+  {
+    files: ['**/*.ts'],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.stylistic,
+      ...angular.configs.tsRecommended,
+    ],
+    processor: angular.processInlineTemplates,
+    rules: {
+      '@angular-eslint/directive-selector': ['error', { type: 'attribute', prefix: 'app', style: 'camelCase' }],
+      '@angular-eslint/component-selector': ['error', { type: 'element', prefix: 'app', style: 'kebab-case' }],
+      '@angular-eslint/prefer-standalone': 'error',
+      '@angular-eslint/prefer-output-readonly': 'error',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+    },
+  },
+  {
+    files: ['**/*.html'],
+    extends: [...angular.configs.templateRecommended, ...angular.configs.templateAccessibility],
+    rules: {
+      '@angular-eslint/template/alt-text': 'error',
+      '@angular-eslint/template/elements-content': 'error',
+      '@angular-eslint/template/prefer-control-flow': 'error',
+      '@angular-eslint/template/prefer-self-closing-tags': 'error',
+    },
+  },
+  eslintConfigPrettier,
+);
