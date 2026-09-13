ca47671aae4a39dbc82d8e78771a1fb67da7e9ca
Anderson Guedes <anderson.guedes@outlook.com.br>
2026-09-13 12:04:21 -0300

    fix(shared): alinhar constants às regras do Prettier

--- src/app/shared/constants/messages.constants.ts
+++ src/app/shared/constants/messages.constants.ts
@@ -58,7 +58,8 @@ export const ToastMessages = {
   CANCELED_REGISTRATION: 'Registro cancelado.',
   SUCCESS_EXPORT: 'Exportação concluída com sucesso.',
   SESSION_WILL_EXPIRE_SOON: 'Sua sessão irá expirar em breve.',
-  SUM_TOTAL_VALUES_ITEMS_DIFFERENT_TOTAL_VALUE: 'A soma dos valores totais dos itens não confere com o valor total da entrada.',
+  SUM_TOTAL_VALUES_ITEMS_DIFFERENT_TOTAL_VALUE:
+    'A soma dos valores totais dos itens não confere com o valor total da entrada.',
   CEP_NOT_FOUND: 'CEP não encontrado. Por favor, verifique o número digitado.',
   CEP_CHECK_ERROR: 'Erro ao consultar o CEP. Tente novamente.',
   CNPJ_INVALID: 'O CNPJ digitado é inválido. Por favor, verifique.',
