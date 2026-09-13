30d5a3470ac842548c4f80239ebad3f2a091aa38
Anderson Guedes <anderson.guedes@outlook.com.br>
2026-09-13 12:01:32 -0300

    fix(shared): alinhar pipes às regras do Prettier

--- src/app/shared/pipes/stock-movement-type/stock-movement-type.pipe.ts
+++ src/app/shared/pipes/stock-movement-type/stock-movement-type.pipe.ts
@@ -1,7 +1,7 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'stockMovementType'
+  name: 'stockMovementType',
 })
 export class StockMovementTypePipe implements PipeTransform {
   transform(value: number): string {
